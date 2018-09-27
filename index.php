c8435fe120fe81fc3ed7c68c081b4c13d9573953
Kevin Sedano Carrillo <ksedanocarrillo@iesesteveterradas.cat>
2018-09-27 17:59:36 +0200

    tercer commit

--- index.php
+++ index.php
@@ -5,9 +5,21 @@
 	<h1 align="center">Calendario</h1>
 </head>
 <body>
+<table>
 	<?php
-		
-	
+	$filas=6;
+	$columnas=7;
+	$num=1;
+
+		for($f=0;$f<$filas;$f++){
+			echo "<tr>";
+			for($c=0;$c<$columnas;$c++){
+				echo "<td>$num</td>";
+				$num+=1;
+			}
+			echo "</tr>";
+		}
 	?>
+</table>
 </body>
 </html>
