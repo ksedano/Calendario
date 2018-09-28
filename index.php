8c95ed9fc4bfe57b33494900e11ce0784fd98bea
Kevin Sedano Carrillo <ksedanocarrillo@iesesteveterradas.cat>
2018-09-28 17:09:47 +0200

    quinto commit

--- index.php
+++ index.php
@@ -5,15 +5,16 @@
 	<h1 align="center">Calendario</h1>
 </head>
 <body>
+	<link rel="stylesheet" type="text/css" href="calendario.css" />
 <table align="center">
-	<tr>
-		<td>Lunes</td>
-		<td>Martes</td>
-		<td>Miercoles</td>
-		<td>Jueves</td>
-		<td>Viernes</td>
-		<td>Sábado</td>
-		<td>Domingo</td>
+	<tr align="center">
+		<td><b>Lunes</b></td>
+		<td><b>Martes</b></td>
+		<td><b>Miercoles</b></td>
+		<td><b>Jueves</b></td>
+		<td><b>Viernes</b></td>
+		<td><b>Sábado</b></td>
+		<td><b>Domingo</b></td>
 	</tr>
 	<?php
 	$filas=6;
@@ -21,7 +22,7 @@
 	$num=1;
 
 		for($f=0;$f<$filas;$f++){
-			echo "<tr>";
+			echo "<tr align='center'>";
 			for($c=0;$c<$columnas;$c++){
 				if($num<=31){
 					echo "<td>$num</td>";
