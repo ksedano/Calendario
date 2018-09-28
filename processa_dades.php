df430fd3a0686374d2559092420386cd9b0d954b
Kevin Sedano Carrillo <ksedanocarrillo@iesesteveterradas.cat>
2018-09-28 19:32:08 +0200

    noveno commit

--- processa_dades.php
+++ processa_dades.php
@@ -17,6 +17,13 @@
 		echo "mycheckbox es: $var4\n</br>";
 		echo "myselect es: $var5\n</br>";
 		echo "mytextarea es: $var6\n</br>";
-	?>
+		
+		print_r($var2.'<br>');
+		print_r($var3.'<br>');
+		print_r($var4.'<br>');
+		print_r($var5.'<br>');
+		print_r($var6.'<br>');
+
+	?>	
 </body>
 </html>
