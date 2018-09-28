05f3395c32dd114ae7201adee52a8f00c77c614d
Kevin Sedano Carrillo <ksedanocarrillo@iesesteveterradas.cat>
2018-09-28 16:54:15 +0200

    cuarto commit

--- index.php
+++ index.php
@@ -5,7 +5,16 @@
 	<h1 align="center">Calendario</h1>
 </head>
 <body>
-<table>
+<table align="center">
+	<tr>
+		<td>Lunes</td>
+		<td>Martes</td>
+		<td>Miercoles</td>
+		<td>Jueves</td>
+		<td>Viernes</td>
+		<td>Sábado</td>
+		<td>Domingo</td>
+	</tr>
 	<?php
 	$filas=6;
 	$columnas=7;
@@ -14,8 +23,10 @@
 		for($f=0;$f<$filas;$f++){
 			echo "<tr>";
 			for($c=0;$c<$columnas;$c++){
-				echo "<td>$num</td>";
-				$num+=1;
+				if($num<=31){
+					echo "<td>$num</td>";
+					$num+=1;
+				}
 			}
 			echo "</tr>";
 		}
---
+++ treball_sencill_formularis.php
@@ -0,0 +1,14 @@
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Formularis</title>
+	<h1 align="center">Formularis</h1>
+</head>
+<body>
+<form align="center">
+	<?php
+	echo "<input id="nom" type="text" name="nom"></input>";
+	?>
+</form>
+</body>
+</html>
